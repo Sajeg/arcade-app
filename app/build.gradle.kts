cc91f1aaf7be0ac5d8f45b78bbebb7b51383e0e5
Sajeg <84874195+Sajeg@users.noreply.github.com>
2024-07-06 14:31:34 +0200

    pushed Version to v1.1 (2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.sajeg.arcade"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
